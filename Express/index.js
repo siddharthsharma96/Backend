7330502d561a6d310c09587dddd4c0f33a8ba57f
siddharthsharma96 <sid9nov@gmail.com>
2024-01-09 11:57:01 +0530

    Added all the api with the array with get getbyid put ,post ,delete

--- Express/index.js
+++ Express/index.js
@@ -1,8 +1,120 @@
 const express = require("express");
-const app = express();
+const bodyparser = require("body-parser");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const app = express();
+app.use(bodyparser.json());
+const details = [
+  {
+    id: 0,
+    date: "2024-01-20",
+    title: "Fee",
+    desc: "Paid the fee",
+    type: 2,
+    currency: 200,
+  },
+  {
+    id: 1,
+    date: "2024-04-20",
+    title: "Travell",
+    desc: "Travel Expenses ",
+    type: 1,
+    currency: 50000,
+  },
+  {
+    id: 2,
+    date: "2024-01-20",
+    title: "Salary",
+    desc: "Salary of SDE ONLY ",
+    type: 2,
+    currency: 200000,
+  },
+];
+
+//   CRUD: C:create (Post) R:Read(get) U:Update(put,patch) D:Delete
+
+app.get("/Expense", async (req, res) => {
+  try {
+    res.status(200).json(details);
+  } catch (err) {
+    res.status(500).json("message:No data found");
+  }
+});
+app.get("/Expense/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    const data = details.find((e) => e.id === parseInt(id, 10));
+    console.log(data);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json("message:No data found");
+  }
+});
+app.post("/Expense", async (req, res) => {
+  try {
+    let { date, type, desc, currency, title } = req.body;
+    console.log(date, type, desc, currency, title);
+    const maxid = details[details.length - 1];
+    console.log(maxid.id);
+    const newExpense = {
+      id: maxid + 1,
+      date,
+      type,
+      desc,
+      currency,
+      title,
+    };
+    details.push(newExpense);
+    res.json("message:Data Added");
+  } catch (err) {
+    res.status(500).json("message:No data found");
+  }
+});
+app.put("/Expense/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    let { date, type, desc, currency, title } = req.body;
+    const expenseIndex = details.findIndex(
+      (detail) => detail.id === parseInt(id, 10)
+    );
+
+    if (expenseIndex === -1) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    console.log(expenseIndex);
+    details[expenseIndex] = {
+      id: parseInt(id, 10),
+      date,
+      title,
+      desc,
+      type,
+      currency,
+    };
+    res.json("message:Success");
+  } catch (err) {
+    res.status(500).json("message:No data found");
+  }
+});
+app.delete("/Expense/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    let { date, type, desc, currency, title } = req.body;
+    const deleteIndex = details.findIndex(
+      (detail) => detail.id === parseInt(id, 10)
+    );
+
+    if (deleteIndex === -1) {
+      return res.status(404).json({ message: "Expense not found" });
+    }
+    console.log(deleteIndex);
+    const deleteExpense = details.splice(deleteIndex, 1);
+
+    res.json("message:Deleted the expense");
+  } catch (err) {
+    res.status(500).json("message:No data found");
+  }
 });
 
 app.listen(3000, () => {
